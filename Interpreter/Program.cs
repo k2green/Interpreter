4cdce944ce44a7225d1843dc00b7b1db7d873420
k2green <kyle.green999@gmail.com>
2020-06-24 22:32:15 +0100

    Re-added Console project
    Re added the console project to the repo

---
+++ Interpreter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InterpreterLib;
+using InterpreterLib.Binding;
+using InterpreterLib.Runtime;
+
+namespace Interpreter {
+	class Program {
+		static void Main(string[] args) {
+			string input = null;
+			bool running = true;
+
+			Dictionary<BoundVariable, object> variables = new Dictionary<BoundVariable, object>();
+
+			Console.CancelKeyPress += delegate {
+				Environment.Exit(0);
+			};
+
+			BindingEnvironment env = null;
+
+			while (running) {
+				input = Console.ReadLine();
+				/*StringBuilder builder = new StringBuilder();
+				while (!(input = Console.ReadLine()).Equals("#quit")) {
+					builder.Append(input);
+				}*/
+
+				if (env == null)
+					env = new BindingEnvironment(input, false);
+				else
+					env = env.ContinueWith(input);
+
+				var output = env.Evaluate(variables);
+
+				if (output.Value != null)
+					Console.WriteLine(output.Value);
+
+				foreach (var message in output.Diagnostics)
+					Console.WriteLine(message);
+			}
+		}
+	}
+}
